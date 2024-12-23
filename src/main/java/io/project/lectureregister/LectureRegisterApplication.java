7f648052df623df0bb7b1debf28c5c9076bb6096
depark <mem29238@gmail.com>
2024-12-23 17:23:13 +0900

    first commit

---
+++ src/main/java/io/project/lectureregister/LectureRegisterApplication.java
@@ -0,0 +1,13 @@
+package io.project.lectureregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureRegisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/project/lectureregister/LectureRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.lectureregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
